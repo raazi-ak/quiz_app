62818cf1da5946a3bce88ba41ead66a714245404
Raazi Faisal <raazi.faisal@gmail.com>
2023-10-27 11:04:02 +0530

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/material.dart';
-import 'image_body.dart';
+import 'package:flutter/services.dart';
+import 'package:quiz_app/quiz.dart';
 
 void main() {
-  runApp(
-    const MaterialApp(
-      home: Scaffold(
-        body: ImageContainer(),
-      ),
-    ),
-  );
+  SystemChrome.setSystemUIOverlayStyle(const SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          Color.fromARGB(255, 31, 95, 89) // Set your desired color here
+      ));
+
+  runApp(const Quiz());
 }
---
+++ lib/quiz.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+import 'package:quiz_app/image_body.dart';
+import 'package:quiz_app/questions_screen.dart';
+
+class Quiz extends StatefulWidget {
+  const Quiz({super.key});
+  @override
+  State<Quiz> createState() {
+    return _QuizState();
+  }
+}
+
+class _QuizState extends State<Quiz> {
+  Widget? activeScreen;
+
+  @override
+  void initState() {
+    activeScreen = ImageContainer(switchScreen);
+    super.initState();
+  }
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
+  @override
+  Widget build(context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 40, 131, 122),
+                Color.fromARGB(255, 31, 95, 89)
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+            ),
+          ),
+          child: activeScreen,
+        ),
+      ),
+    );
+  }
+}
